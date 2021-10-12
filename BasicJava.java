1b9fc81689010d5c16a2175626f53410449fd938
Rani Fanika <ranifanika09@gmail.com>
2021-10-12 13:23:45 +0700

    first commit

---
+++ BasicJava.java
@@ -0,0 +1,22 @@
+public class BasicJava {
+    public static void main(String args []) {
+        String firstName = "Rani ";
+        String lastName = "Fanika";
+        String address = "Dusun IX Bintang Bulan";
+        int age = 20;
+        String tahun = " Tahun";
+        String phoneNumber = "082272157251";
+        Boolean single = true;
+        String hobi = "Membaca";
+        String favoriteFood= "Bakso";
+        
+        System.out.println("First name     : " + firstName + lastName);
+        System.out.println("Address        : " + address);
+        System.out.println("Age            : " + age + tahun);
+        System.out.println("Phone Number   : " + phoneNumber);
+        System.out.println("Hobi           : " + hobi);
+        System.out.println("Single         : " + single);
+        System.out.println("Favorite Food  : " + favoriteFood);
+
+    }
+}
